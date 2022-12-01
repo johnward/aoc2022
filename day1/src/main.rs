6b496217ae3059a517fd028150c1c2ccc1d6164c
John Ward <john.ward@samssonvt.com>
2022-12-01 09:33:53 +0000

    Moved to a function

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,13 @@
 use common::{read_lines};
 fn main() {
-    println!("Hello, world!");
 
-    //let result = write_file("a_path.txt", "Hello".as_bytes()).expect("Write Error");
+    day1_part1();
+}
 
+
+fn day1_part1() {
     let mut highest: u32 = 0;
     let mut elf_cal: u32 = 0;
-
-    println!("elf_cal (initial) {}", elf_cal);
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
@@ -25,12 +25,9 @@ fn main() {
 
                     elf_cal = 0;
                 }
-
-                println!("elf_cal (back to zero): {}", elf_cal);
             }
         }
     }
 
     println!("Highest Cal: {}",highest);
-
 }
