d6b0f682bae5fd23e28dcc5ba3eb56a7d06febfe
John Ward <john.ward@samssonvt.com>
2022-12-03 22:27:26 +0000

    Completed Day3 Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,7 +2,9 @@ use common::{alphabet_position};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
-    part1()?;
+    //part1()?;
+
+    part2()?;
 
     Ok(())
 }
@@ -49,6 +51,73 @@ fn part1() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 
+}
+
+
+fn part2() -> Result<(), Box<dyn std::error::Error>> {
+
+    let content = std::fs::read_to_string("input.txt")?;
+
+    let mut elf1= String::from("a");
+    let mut elf2= String::from("b");
+    let mut elf3= String::from("c");
+
+    let mut count = 0;
+    let mut total = 0;
+
+    
+    for line in content.lines() {
+
+        match count {
+            0 => {
+                elf1 = String::from(line);
+                println!("elf1: {}", elf1);
+            },
+            1 => {
+                elf2 = String::from(line);
+                println!("elf2: {}", elf2);
+            },
+            2 => {
+                elf3 = String::from(line);
+                println!("elf3: {}", elf3);
+                // do the searching here
+
+                for txt in elf1.chars() {
+                    //println!("char: {}", txt);
+
+                    let elf2_find = find_char(txt, &elf2);
+                    let elf3_find = find_char(txt, &elf3);
+
+                    if elf2_find != None && elf3_find != None {
+                        println!("Badge: {}", txt);
+
+                        let mut tmp = [0; 4];
+                        let mut pos = alphabet_position(txt.encode_utf8(&mut tmp)).unwrap();
+
+                        if txt.is_uppercase() {
+                            pos = pos + 26;
+                        }
+
+                        total += pos;
+                        break;
+                    }
+                }
+            },
+            _ => println!("Error"),
+        }
+        if count == 2 {
+            count = 0;
+        } else {
+            count += 1;
+        }        
+    }
+    println!("Total: {}", total);
+
+    Ok(())
+}
+
+fn find_char(a_char: char, a_str: &String) -> Option<usize> {
+    a_str.find(a_char)
 }
 
 
