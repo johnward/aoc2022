debf3aa80941357e1d931d1cd85f270d1a5cc42b
John Ward <john.ward@samssonvt.com>
2022-12-07 00:04:05 +0000

    Completed Day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,7 @@
-fn main() {
+fn main() -> Result<(), Box< dyn std::error::Error>> {
     println!("Hello, world!");
+
+    part2()
 }
 
 
@@ -13,7 +15,7 @@ struct CrateStacks {
     vec7: Vec<char>,
     vec8: Vec<char>,
     vec9 : Vec<char>,
-};
+}
 
 impl CrateStacks {
     pub fn new() -> CrateStacks {
@@ -27,24 +29,115 @@ impl CrateStacks {
         vec8: vec!['D','L','V','Z','R','H','Q'],
         vec9: vec!['B','H','G','N','F','Z','L','D'],}
     }
+
+    pub fn move_single_stack(&mut self, number: i32, from: i32, to: i32) {
+        
+        for _x in 0..number {
+            let value = self.get_vec(from).pop();
+            self.get_vec(to).push(value.unwrap());
+        }
+    }
+
+    pub fn move_multiple_stack(&mut self, number: i32, from: i32, to: i32) {
+        let mut temp_vec = Vec::new();
+
+        for _x in 0..number {
+            let value = self.get_vec(from).pop();
+            temp_vec.push(value.unwrap());
+        }
+
+        for _x in 0..number {
+            self.get_vec(to).push(temp_vec.pop().unwrap());
+        }
+
+    }
+
+    fn get_vec(&mut self, vec_number: i32) -> &mut Vec<char> {
+        
+        match vec_number {
+            1 => return &mut self.vec1,
+            2 => return &mut self.vec2,
+            3 => return &mut self.vec3,
+            4 => return &mut self.vec4,
+            5 => return &mut self.vec5,
+            6 => return &mut self.vec6,
+            7 => return &mut self.vec7,
+            8 => return &mut self.vec8,
+            9 => return &mut self.vec9,
+            _ => return &mut self.vec1,
+        }
+
+    }
+
+    pub fn get_top_elements(&mut self) -> String {
+        let mut top_string = String::from(self.get_vec(1)[0]);
+        top_string.push(*self.get_vec(2).last().unwrap());
+        top_string.push(*self.get_vec(3).last().unwrap());
+        top_string.push(*self.get_vec(4).last().unwrap());
+        top_string.push(*self.get_vec(5).last().unwrap());
+        top_string.push(*self.get_vec(6).last().unwrap());
+        top_string.push(*self.get_vec(7).last().unwrap());
+        top_string.push(*self.get_vec(8).last().unwrap());
+        top_string.push(*self.get_vec(9).last().unwrap());
+
+        top_string
+    }
+
 }
 
 fn part1() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut stacks = CrateStacks::new();
+
     let content = std::fs::read_to_string("input.txt")?;
 
-    // let vec1 = Vec!["H","T","Z","D"];
-    // let vec2 = Vec!["Q","R","W","T","G","C","S"];
-    // let vec3 = Vec!["P","B","F","Q","N","R","C","H"];
-    // let vec4 = Vec!["L","C","N","F","H","Z"];
-    // let vec5 = Vec!["G","L","F","Q","S"];
-    // let vec6 = Vec!["V","P","W","Z","B","R","C","S"];
-    // let vec7 = Vec!["Z","F","J"];
-    // let vec8 = Vec!["D","L","V","Z","R","H","Q"];
-    // let vec9 = Vec!["B","H","G","N","F","Z","L","D"];
+    let lines_processed: i32 = content.lines().map(|line| {
+        let line1 = line.replace("move ", "");
+        let line2 = line1.replace("from ", "");
+        let line3 = line2.replace("to ", "");
 
-    // let total: i32 = content.lines().map(|line| {
+        let mut values = line3.split(" ");
+        let number = values.next().unwrap().parse::<i32>().unwrap();
+        let from = values.next().unwrap().parse::<i32>().unwrap();
+        let to = values.next().unwrap().parse::<i32>().unwrap();
 
-    // });
+        stacks.move_single_stack(number, from, to);
+
+        1
+    }).sum();
+
+    println!("Lines Processed: {}", lines_processed);
+
+    println!("Top elements: {}", stacks.get_top_elements());
+    
+    Ok(())
+}
+
+fn part2() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut stacks = CrateStacks::new();
+
+    let content = std::fs::read_to_string("input.txt")?;
+
+    let lines_processed: i32 = content.lines().map(|line| {
+        let line1 = line.replace("move ", "");
+        let line2 = line1.replace("from ", "");
+        let line3 = line2.replace("to ", "");
+
+        let mut values = line3.split(" ");
+        let number = values.next().unwrap().parse::<i32>().unwrap();
+        let from = values.next().unwrap().parse::<i32>().unwrap();
+        let to = values.next().unwrap().parse::<i32>().unwrap();
+
+        stacks.move_multiple_stack(number, from, to);
+
+        1
+    }).sum();
+
+    println!("Lines Processed: {}", lines_processed);
+
+    println!("Top elements: {}", stacks.get_top_elements());
+    
     Ok(())
 }
 
