1beb1feac239ed4e22ecfdfc4930fde7da3af997
John Ward <john.ward@samssonvt.com>
2022-12-10 10:54:03 +0000

    Initial Project

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::PathBuf;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+
+    let mut manager = PacketManager::new();
+    manager.read_input("input.txt".into());
+
+    Ok(())
+}
+
+struct PacketManager {
+    packets: String,
+    marker: Vec<String>
+}
+
+impl PacketManager {
+    pub fn new() -> PacketManager {
+        PacketManager {
+            packets: String::new(),
+            marker: Vec::new(),
+        }
+    }
+
+    fn read_input(&mut self, file_path: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+        self.packets = std::fs::read_to_string(file_path)?;
+
+        Ok(())
+    }
+
+    pub fn process(&self) {
+
+    }
 }
