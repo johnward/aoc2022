c635f7d743cb450541e58019aa9f5225e52fad96
John Ward <john.ward@samssonvt.com>
2022-12-11 20:00:15 +0000

    Day 6 part 1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -10,6 +10,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut manager = PacketManager::new();
     manager.read_input("input.txt".into());
 
+    let (found, index) = manager.process();
+
+    println!("Index found ({}): {}", found, index);
+
     Ok(())
 }
 
@@ -32,23 +36,49 @@ impl PacketManager {
         Ok(())
     }
 
-    pub fn process(&self) {
+    pub fn process(&self) -> (bool, usize) {
+
+        if self.packets.len() >= 4 {
+            for (i, c) in self.packets.chars().enumerate() {
+                if self.unique_chars(&self.packets[i..i+4]) {
+                    return (true, i + 4);
+                }
+
+                if i == self.packets.len() - 4 {
+                    break;
+                }
+            }
+        }   
+
+        return (false, 0);
 
     }
 
-    fn check_unique(&self) -> Result<bool, Box<dyn std::error::Error>> {
+    fn unique_chars(&self, marker_str: &str) -> bool {
 
-        for m in &self.marker_check {
-            for n in &self.marker_check {
+        //for c in marker_str.chars()
 
-                if n == m {
-                    return Ok(false)
+        if marker_str.eq("abcd") {
+            println!("abcd");
+        }
+
+        println!("Marker Str: {}", marker_str);
+        
+        for (i,m) in marker_str.chars().enumerate() {
+            for (j, n) in marker_str.chars().enumerate() {
+
+                if i == j {
+                    continue;
+                }
+
+                if n.eq_ignore_ascii_case(&m) {
+                    return false
                 }
 
             }
         }
 
-        Ok(true)
+        true
     }
 
 // in C++
