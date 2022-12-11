a1dfac1918ad2b4d1296b7fb2ae077b3e4656935
John Ward <john.ward@samssonvt.com>
2022-12-11 20:03:38 +0000

    tidied up

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,3 @@
-use std::io;
-use std::io::prelude::*;
-use std::fs::File;
 use std::path::PathBuf;
 
 
@@ -8,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
     let mut manager = PacketManager::new();
-    manager.read_input("input.txt".into());
+    let _result = manager.read_input("input.txt".into());
 
     let (found, index) = manager.process();
 
@@ -19,14 +16,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 struct PacketManager {
     packets: String,
-    marker_check: Vec<char>
 }
 
 impl PacketManager {
     pub fn new() -> PacketManager {
         PacketManager {
             packets: String::new(),
-            marker_check: Vec::new(),
         }
     }
 
@@ -39,7 +34,7 @@ impl PacketManager {
     pub fn process(&self) -> (bool, usize) {
 
         if self.packets.len() >= 4 {
-            for (i, c) in self.packets.chars().enumerate() {
+            for (i, _c) in self.packets.chars().enumerate() {
                 if self.unique_chars(&self.packets[i..i+4]) {
                     return (true, i + 4);
                 }
@@ -56,13 +51,7 @@ impl PacketManager {
 
     fn unique_chars(&self, marker_str: &str) -> bool {
 
-        //for c in marker_str.chars()
-
-        if marker_str.eq("abcd") {
-            println!("abcd");
-        }
-
-        println!("Marker Str: {}", marker_str);
+        //println!("Marker Str: {}", marker_str);
         
         for (i,m) in marker_str.chars().enumerate() {
             for (j, n) in marker_str.chars().enumerate() {
@@ -81,29 +70,4 @@ impl PacketManager {
         true
     }
 
-// in C++
-// bool uniqueCharacters(string str)
-// {
- 
-//     // If at any time we encounter 2
-//     // same characters, return false
-//     for (int i = 0; i < str.length() - 1; i++) {
-//         for (int j = i + 1; j < str.length(); j++) {
-//             if (str[i] == str[j]) {
-//                 return false;
-//             }
-//         }
-//     }
- 
-    // If no duplicate characters encountered,
-    // return true
-//     return true;
-// }
-
-    pub fn add_marker(&mut self, marker_char: char) -> Result<(), Box<dyn std::error::Error>> {
-
-        self.marker_check.push(marker_char);
-
-        Ok(())
-    }
 }
